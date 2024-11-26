d235822f752d7143b40f12adc735ce2c1d24f26c
akeley10 <alejandmar10@gmail.com>
2024-11-26 23:14:09 +0000

    añadiendo funcionalidad botones y diseño

--- src/Reproductor.jsx
+++ src/Reproductor.jsx
@@ -1,27 +1,51 @@
 import * as React from 'react';
 import {useState} from 'react'
 import ReactPlayer from 'react-player';
-
-
-
-
-
+import '../src/reproductor.css'
 
 
 const Reproductor = () => {
+  const [url ,setUrl] = useState("Pon una url");
+  const [playing, setPlaying] = useState(false);
+  const [videoState, setVideoState] = useState('Video no started');
+
+
+  const handleStart = () => {
+    setVideoState('playing');
+  }
+
+  const handlePlay = () => {
+    setPlaying(true);
+    setVideoState('playing');
+  }
+
+  const handlePause = () => {
+    setPlaying(false);
+    setVideoState('Paused');
+  }
+
   return (
     <>
-   
-    <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ'
-    controls
+    <p>Video Status: {videoState}</p>
+    <ReactPlayer url={url}
     width={1000}
     height={500} 
-    loop={false}
+    playing={playing}
+    controls={true}
+    onStart={handleStart}
+    onPlay={handlePlay}
+    onPause={handlePause}
+    
     />
-     <button>Change Video</button>
-     <button>Start</button>
-     <button>Pause</button>
-     <button>Reset</button>
+    <div class="menu-reproductor">
+    <input
+    placeholder="Pon una url"
+    type="text"
+    value={url}
+    onChange={(e) => setUrl(e.target.value)}
+   />
+     <button onClick={() =>setPlaying(!playing)}>{playing ? 'Pause':'Play'}</button>
+     </div>
     </>
   )
 
