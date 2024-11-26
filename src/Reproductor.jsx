19d19a94f9fb6d505b265398eb3ffcf607edbd50
akeley10 <alejandmar10@gmail.com>
2024-11-26 13:09:09 +0000

    añadiendo botones

--- src/Reproductor.jsx
+++ src/Reproductor.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import * as React from 'react';
+import {useState} from 'react'
 import ReactPlayer from 'react-player';
+
+
+
+
+
+
 
 const Reproductor = () => {
   return (
+    <>
+   
     <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ'
     controls
-    width={1080}
-    height={1000} />
+    width={1000}
+    height={500} 
+    loop={false}
+    />
+     <button>Change Video</button>
+     <button>Start</button>
+     <button>Pause</button>
+     <button>Reset</button>
+    </>
   )
+
 }
 
 export default Reproductor
